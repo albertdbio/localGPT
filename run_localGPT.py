a436ac2cc392acdca56ef82da39fd500cba294bb
Albert Martinez <ioalbertdb@gmail.com>
2023-06-02 16:40:53 -0700

    Make non interactive

--- run_localGPT.py
+++ run_localGPT.py
@@ -40,8 +40,9 @@ def load_model():
     return local_llm
 
 @click.command()
+@click.option('--query', prompt='Enter a query', help='Query to be answered')
 @click.option('--device_type', default='gpu', help='device to run on, select gpu or cpu')
-def main(device_type, ):
+def main(query, device_type):
     # load the instructorEmbeddings
     if device_type in ['cpu', 'CPU']:
         device='cpu'
@@ -60,28 +61,23 @@ def main(device_type, ):
     # load the LLM for generating Natural Language responses. 
     llm = load_model()
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-    # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
         
-        # Get the answer from the chain
-        res = qa(query)    
-        answer, docs = res['result'], res['source_documents']
+    # Get the answer from the chain
+    res = qa(query)    
+    answer, docs = res['result'], res['source_documents']
 
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print("\n> Answer:")
-        print(answer)
-        
-        # # Print the relevant sources used for the answer
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
+    # Print the result
+    print("\n\n> Question:")
+    print(query)
+    print("\n> Answer:")
+    print(answer)
+    
+    # # Print the relevant sources used for the answer
+    print("----------------------------------SOURCE DOCUMENTS---------------------------")
+    for document in docs:
+        print("\n> " + document.metadata["source"] + ":")
+        print(document.page_content)
+    print("----------------------------------SOURCE DOCUMENTS---------------------------")
 
 
 if __name__ == "__main__":
